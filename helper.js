bf3cb91baa789998d2681eebf1bff0ded6645605
Darafei Praliaskouski <me@komzpa.net>
2012-05-05 16:00:45 +0400

    russian lang support

--- helper.js
+++ helper.js
@@ -3,6 +3,10 @@
 
 josm_remote = false;
 
+if (!$.cookie('userid')){
+  $.cookie('userid', new Date().getTime()/1000, {expires: 10});
+};
+
 $.getJSON('http://localhost:8111/version?jsonp=?', function(data){josm_remote = data;});
 
 $().ready( function(){
@@ -12,8 +16,17 @@ $().ready( function(){
   
   var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-be/{z}/{x}/{y}.png';
   
-  if (locale == 'en'){var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-en/{z}/{x}/{y}.png';}
-  if ((locale != 'en') && (locale != 'be')) {var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-blank/{z}/{x}/{y}.png';  }
+  if (locale == 'en'){
+    var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-en/{z}/{x}/{y}.png';
+  } else if (locale == 'ru') {
+    var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo/{z}/{x}/{y}.png';
+  } else if (locale == 'be') {
+    var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-be/{z}/{x}/{y}.png';
+  } else if (locale == 'none') {
+    var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-blank/{z}/{x}/{y}.png';
+  }
+
+
   
   kosmo = new L.TileLayer(kosmoUrl, {maxZoom: 18, attribution: _("Map data © <a href='http://osm.org'>OpenStreetMap</a> contributors, CC-BY-SA; rendering by <a href='http://kosmosnimki.ru'>kosmosnimki.ru</a>")});
   mmap.addControl(new L.Control.Breadcrumbs());
