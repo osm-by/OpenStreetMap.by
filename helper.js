b4f006c610499ce57d8dca740ee5012479097379
Darafei Praliaskouski <komzpa@gmail.com>
2013-12-29 17:49:29 +0300

    disable non-ru tiles temporarily

--- helper.js
+++ helper.js
@@ -4,8 +4,6 @@ if (google.loader.ClientLocation){
   geopoint = mmap.getCenter();
 }
 
-//from_geolocation = true;
-
 josm_remote = false;
 
 if (!$.cookie('userid')){
@@ -19,9 +17,9 @@ $().ready( function(){
   mmap.attributionControl.setPrefix("");
   
   
-  var kosmoUrl = 'http://{s}.tile.openstreetmap.by:3128/osmby-be/{z}/{x}/{y}.png';
-  
-  if (locale == 'en'){
+  var kosmoUrl = 'http://{s}.tile.openstreetmap.by:3128/osmby-ru/{z}/{x}/{y}.png';
+/*  
+   if (locale == 'en'){
     var kosmoUrl = 'http://{s}.tile.openstreetmap.by:3128/osmby-en/{z}/{x}/{y}.png';
   } else if (locale == 'ru') {
     var kosmoUrl = 'http://{s}.tile.openstreetmap.by:3128/osmby-ru/{z}/{x}/{y}.png';
@@ -30,7 +28,7 @@ $().ready( function(){
   }// else if (locale == 'none') {
     //var kosmoUrl = 'http://{s}.tile.osmosnimki.ru/kosmo-blank/{z}/{x}/{y}.png';
  // }
-
+*/
   kosmo = new L.TileLayer(kosmoUrl, {maxZoom: 18, attribution: _("Map data © <a href='http://osm.org'>OpenStreetMap</a> contributors")});
   //mmap.addControl(new L.Control.Breadcrumbs());
   
