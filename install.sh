b7fc482b56ee005892d9b1b492623d19211037f2
Paveł Tyślacki <pavel.tysliatski@datarobot.com>
2021-03-12 16:28:21 +0300

    update openmaptiles project reference temp fix for https://github.com/openmaptiles/openmaptiles/issues/967

--- install.sh
+++ install.sh
@@ -12,16 +12,6 @@ apt-get install wget unzip patch gawk bc build-essential python3 python3-venv po
 docker info
 
 
-# install openmaptiles
-wget https://github.com/openmaptiles/openmaptiles/archive/master.zip
-unzip -o *.zip
-rm -f *.zip
-mv openmaptiles-* openmaptiles
-cd openmaptiles
-patch < ../updater/openmaptiles.patch
-cd ..
-
-
 # create venv
 python3 -m venv venv
 venv/bin/pip3 install wheel
